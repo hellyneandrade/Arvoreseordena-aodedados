ea7ab6410c0fb98bf0c4c8432583949ca3c08a84
hellyneandrade <hellyneandrade@gmail.com>
2026-09-02 11:12:43 -0300

    Add header file for binary tree structure and functions

--- meuprojeto/arvore.h
+++ meuprojeto/arvore.h
@@ -1,1 +1,13 @@
+#fndef ARVORE_H
+#define ARVORE_H
 
+typedef struct arv Arv;
+
+Arv* arv_criavazia(void);
+Arv* arv_cria(char c, Arv* sae, Arv* sad);
+int arv_vazia(Arv* a);
+int arv_pertence(Arv* a, char c);
+Arv* arv_libera(Arv* a);
+void arv_imprime(Arv* a);
+
+#endif
