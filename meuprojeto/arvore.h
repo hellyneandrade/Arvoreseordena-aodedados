4ecaed9c34b289b8e6197d1053d46fa1129f8648
hellyneandrade <hellyneandrade@gmail.com>
2026-09-02 12:16:32 -0300

    Define binary tree structure and add new functions

--- meuprojeto/arvore.h
+++ meuprojeto/arvore.h
@@ -1,7 +1,11 @@
-#fndef ARVORE_H
+#ifndef ARVORE_H
 #define ARVORE_H
 
-typedef struct arv Arv;
+typedef struct arv {
+    char info;
+    struct arv* esq;
+    struct arv* dir;
+} Arv;
 
 Arv* arv_criavazia(void);
 Arv* arv_cria(char c, Arv* sae, Arv* sad);
@@ -9,5 +13,7 @@ int arv_vazia(Arv* a);
 int arv_pertence(Arv* a, char c);
 Arv* arv_libera(Arv* a);
 void arv_imprime(Arv* a);
+int arv_altura(Arv* a);
+int arv_qtd_nos(Arv* a);
 
 #endif
